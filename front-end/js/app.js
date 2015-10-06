6be5e7dd52054cbca84f8d39b1623844cae5be7e
manaiajunior <manaiajr.ifrn@gmail.com>
2015-10-06 00:18:51 -0300

    Added material angular and add file bower.json.

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,4 +1,11 @@
 (function(angular, undefined) {
 	'use strict';
-	angular.module('cronusApp', ['ngRoute']);
+	angular.module('cronusApp', ['ngRoute', 'ngMaterial', 'ngMdIcons'])
+	  .controller('CronusCtrl', function DemoCtrl($mdDialog) {
+	    var originatorEv;
+	    this.openMenu = function($mdOpenMenu, ev) {
+	      originatorEv = ev;
+	      $mdOpenMenu(ev);
+	    };
+	  });
 })(angular);
